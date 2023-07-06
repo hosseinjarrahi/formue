56d6add50b49cd9d096d23e8a6c37f97ec7b81f6
Haj hossein <hj021hj@gmail.com>
2023-07-06 13:04:14 +0330

    add styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import MForm from "./components/MForm.vue";
 import { useEmitter } from "./composables/useEmitter";
 import FormFieldAbstract from "./components/form/FormFieldAbstract.vue";
 import useFieldProps from "./composables/useFieldProps";
+import './assets/main.css'
 
 export default MForm;
 
